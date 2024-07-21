579fcb8ffce3be2f775b2befc737cd9b7f7d4ba5
Metin Karakus <karakusmeetin@gmail.com>
2024-07-21 16:56:10 +0300

    The project was created and entity was added to the base layer.

---
+++ ET.Base/Entities/BaseEntityWithId.cs
@@ -0,0 +1,14 @@
+﻿namespace ET.Base.Entities
+{
+    public abstract class BaseEntityWithId
+    {
+        public Guid Id { get; set; }
+        public Guid InsertUserId { get; set; }
+        public DateTime InsertDate { get; set; }
+        public Guid? UpdateUserId { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public Guid? DeleteUserId { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
